8a209286b1de4bbb059314191dc1668eb4b6de5c
debuggingfuture <lauchunyin@debuggingfuture.com>
2024-09-20 23:03:38 +0800

    add: placeholder contract

---
+++ packages/contracts/test/Resolver.t.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {OffchainResolver} from "../src/OffchainResolver.sol";
+import {IExtendedResolver} from "../src/IExtendedResolver.sol";
+
+contract ResolverTest is Test {
+    OffchainResolver public resolver;
+
+    function setUp() public {
+        string memory url = "https://example.com";
+        address[] memory signers = new address[](2);
+        signers[0] = 0x1234567890123456789012345678901234567890;
+        signers[1] = 0x0987654321098765432109876543210987654321;
+
+        // Deploy the OffchainResolver contract
+        resolver = new OffchainResolver(url, signers);
+
+    }
+
+    function testInterface(uint256 x) public view {
+        bool result = resolver.supportsInterface(type(IExtendedResolver).interfaceId);
+        assertEq(result, true); // IExtendedResolver
+    }
+
+    function testResolve(uint256 x) public view {
+
+        byte
+        bool result = resolver.resolve(type(IExtendedResolver).interfaceId);
+        assertEq(result, true); // IExtendedResolver
+    }
+}
